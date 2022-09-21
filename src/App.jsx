c8b91ac2824f253e00f422b6772ffbe0ca2061a8
PritamSapkota <slicefrost1@gmail.com>
2022-09-21 14:20:55 -0400

    Change browserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import { HashLink as Link } from 'react-router-hash-link';
 
 
 function App() {
   
   return (
-    <BrowserRouter>
+    <HashRouter>
       <main className='App'> 
         <Navbar />         
         <Aboutme />
@@ -21,7 +21,7 @@ function App() {
         <Contact />
         <Footer />
       </main>
-    </BrowserRouter>
+    </HashRouter>
   )
     
 }
