84123c388f7c9777fa69b4bf43c1a75137a3a52f
PritamSapkota <slicefrost1@gmail.com>
2022-09-21 13:58:18 -0400

    Update link url

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 export default function NavBar() {
     return (
         <nav className='nav-bar' id="navpage">
-            <Link href='#navpage'><h1  smooth className='nav-header'>Pritam Sapkota</h1></Link>
+            <a href='https://pritamsap.com/'><h1  smooth className='nav-header'>Pritam Sapkota</h1></a>
             <ul className='nav-list'>
                 <Link to="#projects" smooth><li>Projects</li></Link>
                 <Link to="#skills" smooth><li>Skills</li></Link>
